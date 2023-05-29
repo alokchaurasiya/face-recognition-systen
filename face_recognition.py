16b41dddda1f9e6407888b9170bae8dfcab1cadc
m-alokchaurasiya-m <alokchaurasiya9565@gmail.com>
2023-05-29 23:37:59 +0530

    face

--- face_recognition.py
+++ face_recognition.py
@@ -89,19 +89,19 @@ class Face_Recognition:
 
 
                 if confidence>77:
-                    cv2.putText(img,f"ID:{i}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
-                    cv2.putText(img,f"Roll:{r}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
-                    cv2.putText(img,f"Name:{n}",(x,y-20),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    cv2.putText(img,f"ID:{i}",(x,y-70),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),1)
+                    cv2.putText(img,f"Roll:{r}",(x,y-45),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),1)
+                    cv2.putText(img,f"Name:{n}",(x,y-15),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),1)
                 else:
                     cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
-                    cv2.putText(img,"Unknown Face",(x,y-15),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    cv2.putText(img,"Unknown Face",(x,y-15),cv2.FONT_HERSHEY_COMPLEX,0.8,(220,20,60),1)
 
                 coord=[x,y,w,y]
 
             return coord
         
         def recognize(img,clf,faceCascade):
-            coord =draw_boundray(img,faceCascade,1.1,10,(255,25,255),"Face",clf)
+            coord =draw_boundray(img,faceCascade,1.1,10,(255,25,255),'face',clf)
             return img
         
         faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,17 @@
-cv2.putText(img,f"ID:{i}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                if confidence>77:
+                    cv2.putText(img,f"ID:{i}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    cv2.putText(img,f"Roll:{r}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    cv2.putText(img,f"Name:{n}",(x,y-20),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                else:
+                    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+                    cv2.putText(img,"Unknown Face",(x,y-15),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+
+                coord=[x,y,w,y]
+
+            return coord
+        
+        def recognize(img,clf,faceCascade):
+            coord =draw_boundray(img,faceCascade,1.1,10,(255,25,255),"Face",clf)
+            return img
+        
+        faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.
